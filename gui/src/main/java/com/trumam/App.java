b31c5cd9121f51822cff664a149d23e5ea47b97c
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-07-24 19:33:41 -0500

    Modules and GUI test

---
+++ gui/src/main/java/com/trumam/App.java
@@ -0,0 +1,36 @@
+package com.trumam;
+
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+/**
+ *JavaFX GUI
+ */
+public class App extends Application {
+    /**
+     * Start method
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //-->Create main window
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/MainWindow.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        // Window main = loader.getController();
+        // main.initModel(model);
+        // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
+        primaryStage.setTitle("Kmeans++ distribuido");
+        primaryStage.setScene(scene);
+        //<--Create main window
+        primaryStage.show();
+    }
+    /**
+     * Dummy main method
+     */
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+}
---
+++ gui/src/main/java/com/trumam/MainWindowController.java
@@ -0,0 +1,5 @@
+package com.trumam;
+
+public class MainWindowController {
+	
+}
